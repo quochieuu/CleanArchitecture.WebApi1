c2a261ac89680b6a4c0030c65273c181883e4912
hieuho <quochieuu@gmail.com>
2022-06-18 10:26:14 +0700

    Add project files.

---
+++ CleanArchitecture.WebApi1/CleanArchitecture.WebApi1.WebApi/Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.WebApi1.WebApi
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //Read Configuration from appSettings
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            //Initialize Logger
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    Log.Information("Finished Seeding Default Data");
+                    Log.Information("Application Starting");
+                }
+                catch (Exception ex)
+                {
+                    Log.Warning(ex, "An error occurred seeding the DB");
+                }
+                finally
+                {
+                    Log.CloseAndFlush();
+                }
+            }
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .UseSerilog() //Uses Serilog instead of default .NET Logger
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
